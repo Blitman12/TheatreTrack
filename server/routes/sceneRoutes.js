167527aa2f89a7f2f5b6372dd920562757e3a149
B-Litty <bradlitman94@gmail.com>
2022-07-29 15:22:58 -0700

    added route to create a scene and then add to act

--- server/routes/sceneRoutes.js
+++ server/routes/sceneRoutes.js
@@ -34,11 +34,25 @@ router.post('/scene', async (req, res) => {
     }
 });
 
+router.post('/scene/act/:actId', async (req, res) => {
+    try {
+        const actId = req.params.actId;
+        const scene = new Scene(req.body);
+        await scene.save();
+        const addScene = await Scene.findById(scene._id.toString());
+        await Act.findByIdAndUpdate(actId, { $push: { scenes: addScene } }, { new: true }).then(dbActData => {
+            res.status(200).send(dbActData);
+        });
+    } catch (error) {
+        res.status(500).send(error);
+    }
+})
+
 router.put('/scene/:id', async (req, res) => {
     try {
         const sceneId = req.params.id;
-        await Scene.findByIdAndUpdate(sceneId, req.body, {new: true}).then(dbSceneData => {
-        res.status(200).send(dbSceneData);
+        await Scene.findByIdAndUpdate(sceneId, req.body, { new: true }).then(dbSceneData => {
+            res.status(200).send(dbSceneData);
         })
     } catch (error) {
         res.status(400).send(`An Error Ocurred: ${error}`);
@@ -50,8 +64,8 @@ router.put('/scene/:sceneId/addactor/:actorId', async (req, res) => {
     const sceneId = req.params.sceneId;
     try {
         const actor = await Actor.findById(actorId);
-        await Scene.findByIdAndUpdate(sceneId, {$push: {actors: actor}}, {new: true}).then(dbSceneData => {
-        res.status(200).send(dbSceneData);
+        await Scene.findByIdAndUpdate(sceneId, { $push: { actors: actor } }, { new: true }).then(dbSceneData => {
+            res.status(200).send(dbSceneData);
         })
     } catch (error) {
         res.status(400).send(`An Error Ocurred: ${error}`);
@@ -62,8 +76,8 @@ router.put('/scene/:sceneId/removeactor/:actorId', async (req, res) => {
     const actorId = req.params.actorId;
     const sceneId = req.params.sceneId;
     try {
-        await Scene.findByIdAndUpdate(sceneId, {$pull: {actors: actorId}}, {new: true}).then(dbSceneData => {
-        res.status(200).send(dbSceneData);
+        await Scene.findByIdAndUpdate(sceneId, { $pull: { actors: actorId } }, { new: true }).then(dbSceneData => {
+            res.status(200).send(dbSceneData);
         })
     } catch (error) {
         res.status(400).send(`An Error Ocurred: ${error}`);
