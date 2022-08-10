ce1ba78af364ce68131807ee19b028084835a902
B-Litty <bradlitman94@gmail.com>
2022-08-09 17:38:59 -0700

    home page icon

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { NavigationStart, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
+import { filter, map } from 'rxjs';
 import { ProjectComponent } from 'src/core/project/components/project/project.component';
 import { actorActions } from 'src/core/project/state';
 import { ProjectSelectors } from 'src/core/project/state/selectors';
@@ -15,10 +17,12 @@ import { BaseComponent } from 'src/shared/bases/base.component';
 export class AppComponent extends BaseComponent implements OnInit {
   public title = 'Theatre Track';
   public actorToggleState!: boolean;
+  public url = '';
 
   public constructor(
     private _dialog: MatDialog,
     private _projectSelectors: ProjectSelectors,
+    private _router: Router,
     private _store: Store
     ) { 
       super();
@@ -38,7 +42,11 @@ export class AppComponent extends BaseComponent implements OnInit {
 
   private setupSubscriptions() {
     this._subscriptions.push(
-      this._projectSelectors.actorToggleState.subscribe(state => this.actorToggleState = state)
+      this._projectSelectors.actorToggleState.subscribe(state => this.actorToggleState = state),
+      this._router.events.pipe(
+        filter(event => event instanceof NavigationStart),
+        map(event => event as NavigationStart)
+      ).subscribe(event => this.url = event.url)
     )
   }
 }
